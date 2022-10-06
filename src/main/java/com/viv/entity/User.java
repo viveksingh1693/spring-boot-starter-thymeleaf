cf08a0b0e12dc00e86f5636b79d4b13a1d088231
vivesing <vivek.singh1693@gmail.com>
2022-10-06 23:30:51 +0530

    [thymleaf-001] adding changes for css

--- src/main/java/com/viv/entity/User.java
+++ src/main/java/com/viv/entity/User.java
@@ -4,30 +4,36 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
+@Table(name = "users")
 public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 
-    @NotBlank(message = "Name is mandatory")
+	@NotBlank(message = "Name is mandatory")
 	@Size(min = 2, max = 32, message = "Name must be between 2 and 32 characters long")
-	private String name;
+	private String firstName;
+	
+	@NotBlank(message = "Name is mandatory")
+	@Size(min = 2, max = 32, message = "Name must be between 2 and 32 characters long")
+	private String lastName;
 
-    @NotBlank(message = "Email is mandatory")
+	@NotBlank(message = "Email is mandatory")
 	private String email;
 
 }
